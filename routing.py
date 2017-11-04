6ed3df66f0a4c5ff7ebf5540489c94dc8ccef25b
osanchez <osanchez@umass.edu>
2017-11-04 03:08:26 -0400

    testing database API

--- routing.py
+++ routing.py
@@ -1,4 +1,28 @@
-from flask import Flask, url_for, render_template
-from app import app
+# from flask import Flask, url_for, render_template
+# from app import app
 
 from brewerydb.brewerydb import BreweryDB
+
+
+# API Key:  5ceb8b4ef81887489d3b65211a60fe12
+
+
+class BreweryQuery:
+    def __init__(self, api_key):
+        self._key = api_key
+
+    def get_alcohol_content(self):
+        brew_api = BreweryDB(self._key)
+        beer = brew_api.search_beer("Corona Light")
+        return beer
+
+
+def main():
+    my_api_key = "5ceb8b4ef81887489d3b65211a60fe12"
+    test = BreweryQuery(my_api_key)
+    result = test.get_alcohol_content()
+    print(result)
+
+
+if __name__ == "__main__":
+    main()
--- test.py
+++
@@ -1,3 +0,0 @@
-import BreweryDB
-brew_api = BreweryDB("87b7bbfd7adae6ccf26146cc102436e0")
-beers = brew_api.search_beer('fat tire')
