542646496bcf160ed1e168d6e76417b1484cea0a
Kevin Sandoval <ks777@scarletmail.rutgers.edu>
2017-11-04 02:15:39 -0400

    python work

---
+++ routing.py
@@ -0,0 +1,4 @@
+from flask import Flask, url_for, render_template
+from app import app
+
+from brewerydb.brewerydb import BreweryDB
---
+++ test.py
@@ -0,0 +1,3 @@
+import BreweryDB
+brew_api = BreweryDB("87b7bbfd7adae6ccf26146cc102436e0")
+beers = brew_api.search_beer('fat tire')
